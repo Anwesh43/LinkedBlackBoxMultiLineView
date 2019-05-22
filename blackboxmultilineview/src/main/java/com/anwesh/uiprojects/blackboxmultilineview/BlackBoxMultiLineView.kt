0049c7cad9c4cdf71e9a8f85844d7f8a60c6eafe
Anwesh43 <anweshthecool0@gmail.com>
2019-05-22 09:39:34 +0530

    adding BBMNode class

--- blackboxmultilineview/src/main/java/com/anwesh/uiprojects/blackboxmultilineview/BlackBoxMultiLineView.kt
+++ blackboxmultilineview/src/main/java/com/anwesh/uiprojects/blackboxmultilineview/BlackBoxMultiLineView.kt
@@ -133,8 +133,52 @@ class BlackBoxMultiLineView(ctx : Context) : View(ctx) {
 
         fun stop() {
             if (animated) {
-                animated = false 
+                animated = false
             }
+        }
+    }
+
+    data class BBMLNode(var i : Int, val state : State = State()) {
+
+        private var next : BBMLNode? = null
+        private var prev : BBMLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BBMLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBBMLNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BBMLNode {
+            var curr : BBMLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
         }
     }
 }
