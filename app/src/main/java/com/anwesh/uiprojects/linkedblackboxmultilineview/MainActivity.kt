5e6c25737898d9b790db619f8c2345292039f324
Anwesh43 <anweshthecool0@gmail.com>
2019-05-22 09:54:59 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedblackboxmultilineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedblackboxmultilineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedblackboxmultilineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.blackboxmultilineview.BlackBoxMultiLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BlackBoxMultiLineView.create(this)
     }
 }
