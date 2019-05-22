2bf7b6340218465814218570979975dde37ae3ad
Anwesh43 <anweshthecool0@gmail.com>
2019-05-22 09:41:59 +0530

    adding BlackBoxMultiLine class

--- blackboxmultilineview/src/main/java/com/anwesh/uiprojects/blackboxmultilineview/BlackBoxMultiLineView.kt
+++ blackboxmultilineview/src/main/java/com/anwesh/uiprojects/blackboxmultilineview/BlackBoxMultiLineView.kt
@@ -178,7 +178,31 @@ class BlackBoxMultiLineView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class BlackBoxMultiLine(var i : Int) {
+
+        private val root : BBMLNode = BBMLNode(0)
+        private var curr : BBMLNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
